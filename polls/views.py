d5215ba0ff04efcbaa4346d4a0a090a904feefc8
Abdul Rasid <abdulrasid@Abduls-MacBook-Pro.local>
2019-09-08 23:10:42 +0530

    Finale commit

---
+++ polls/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+	questions = Question.objects.order_by('-pub_date')[:3]
+	return render(request, 'polls/index.html', {'questions':questions})
+
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question':question})
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk = question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {'question':question, 'error_message': 'You did not select a choice',})
+	else:
+		selected_choice.votes += 1 
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
